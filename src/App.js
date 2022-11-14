f9a24aa48611180ff996d84da748211c2e8414cd
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-14 20:30:39 +0100

    SET_STORIES functionality

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <SearchForm />
+      <Buttons />
       <Stories />
-      <Buttons /> 
     </>
   );
 }
--- src/context.js
+++ src/context.js
@@ -13,6 +13,10 @@ const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
 const initialState = {
   isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
 };
 
 const AppContext = React.createContext();
@@ -22,10 +26,20 @@ const AppProvider = ({ children }) => {
 
   const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, []);
 
   return (
