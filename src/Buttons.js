7b04011dd3cc8e8be76695084016c67ee24c1560
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-24 17:05:30 +0100

    Display page number

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,21 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const Buttons = () => {
-  return <h2>button container</h2>
-}
+  const { isLoading, page, nbPages, handlePage } = useGlobalContext();
+  return (
+    <h2 className="btn-container">
+      <button disabled={isLoading} onClick={() => handlePage("dec")}>
+        prev
+      </button>
+      <p>
+        {page + 1} of {nbPages}
+      </p>
+      <button disabled={isLoading} onClick={() => handlePage("inc")}>
+        Next
+      </button>
+    </h2>
+  );
+};
 
-export default Buttons
+export default Buttons;
