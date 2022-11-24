360e9d5aee1d90bbd9c6f6f3c6eb92df20caedb0
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-24 17:28:14 +0100

    Reducer function to increment and decrement pages

--- src/reducer.js
+++ src/reducer.js
@@ -10,7 +10,7 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true };
-    case SET_STORIES: 
+    case SET_STORIES:
       return {
         ...state,
         isLoading: false,
@@ -23,7 +23,22 @@ const reducer = (state, action) => {
         hits: state.hits.filter((story) => story.objectID !== action.payload),
       };
     case HANDLE_SEARCH:
-      return {...state, query:action.payload,page: 0}
+      return { ...state, query: action.payload, page: 0 };
+    case HANDLE_PAGE:
+      if (action.payload === "inc") {
+        let nextPage = state.page + 1;
+        if (nextPage > state.nbPages - 1) {
+          nextPage = 0;
+        }
+        return { ...state, page: nextPage };
+      }
+      if (action.payload === "dec") {
+        let prevPage = state.page - 1;
+        if (prevPage < 0) {
+          prevPage = state.nbPages - 1;
+        }
+        return { ...state, page: prevPage };
+      }
     default:
       throw new Error(`no matching "${action.type}" action type`);
   }
